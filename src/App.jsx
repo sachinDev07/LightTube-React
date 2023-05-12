96f3ed47c76ed76b568199bced9dc2a239e71328
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-05-12 07:29:59 +0500

    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-2xl">Home page</h1>
+      <Header />
     </>
   );
 }
