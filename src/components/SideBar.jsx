c8b7af4f6b15560c33fb2f33caf91946fa32414e
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-05-15 00:32:29 +0500

    Hamburger toggle is done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+
   return (
     <aside className="hidden h-[calc(100%_-_60px)] w-56 bg-black pl-4 pr-4 text-white md:block">
       <ul className="flex flex-col border-b-2 border-solid border-white/[0.4] pb-3">
@@ -19,65 +24,65 @@ const SideBar = () => {
       </ul>
       <ul className="flex flex-col border-b-2 border-solid border-white/[0.4] pb-3 pt-3">
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-photo-film mr-6"></i>
+          <i className="fa fa-photo-film mr-6"></i>
           <span>Library</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-clock-rotate-left mr-6"></i>
+          <i className="fa fa-clock-rotate-left mr-6"></i>
           <span>History</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-play mr-7"></i>
+          <i className="fa fa-play mr-7"></i>
           <span>Your vidoes</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-regular fa-clock mr-6"></i>
+          <i className="fa-regular fa-clock mr-6"></i>
           <span>Watch later</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-solid fa-scissors mr-6"></i>
+          <i className="fa-solid fa-scissors mr-6"></i>
           <span>Your clips</span>
         </Link>
       </ul>
       <span className="bold mt-4 inline-block text-lg text-white">Explore</span>
       <ul className="mt-3 flex flex-col">
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-solid fa-fire mr-6"></i>
+          <i className="fa-solid fa-fire mr-6"></i>
           <span>Trending</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-bag-shopping mr-6"></i>
+          <i className="fa fa-bag-shopping mr-6"></i>
           <span>Shopping</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-music mr-6"></i>
+          <i className="fa fa-music mr-6"></i>
           <span>Music</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-film mr-6"></i>
+          <i className="fa fa-film mr-6"></i>
           <span>Movies & Shows</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-signal mr-6"></i> <span>Live</span>
+          <i className="fa fa-signal mr-6"></i> <span>Live</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa fa-gamepad mr-6"></i>
+          <i className="fa fa-gamepad mr-6"></i>
           <span>Gaming</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-solid fa-newspaper mr-6"></i>
+          <i className="fa-solid fa-newspaper mr-6"></i>
           <span>News</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-solid fa-trophy mr-6"></i>
+          <i className="fa-solid fa-trophy mr-6"></i>
           <span>Sports</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-solid fa-lightbulb mr-6"></i>
+          <i className="fa-solid fa-lightbulb mr-6"></i>
           <span>Learning</span>
         </Link>
         <Link className="transition-bg mb-1 flex items-center  rounded-lg px-3 py-2 duration-150 ease-in-out hover:bg-white/[0.2]">
-          <i class="fa-solid fa-shirt mr-5"></i>
+          <i className="fa-solid fa-shirt mr-5"></i>
           <span>Fashion & Beauty</span>
         </Link>
       </ul>
