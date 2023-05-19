162ddcf42694b347b0eec6baf3784687a4ee65d9
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-05-19 22:10:49 +0500

    Added custom font family and screen sizes for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./src/**/*.{js,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      pathway: ["Pathway Gothic One", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
     extend: {},
+  },
+  screens: {
+    // xs: "340px",
+    sm: "640px",
+    // => @media (min-width: 640px) { ... }
+
+    md: "768px",
+    // => @media (min-width: 768px) { ... }
+
+    lg: "1024px",
+    // => @media (min-width: 1024px) { ... }
+
+    xl: "1280px",
+    // => @media (min-width: 1280px) { ... }
+
+    "2xl": "1536px",
+    // => @media (min-width: 1536px) { ... }
   },
   plugins: [],
 };
