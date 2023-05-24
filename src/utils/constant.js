2a4da222333c9d17c86724fcdf4a0df95a03b614
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-05-24 23:25:56 +0500

    imported youtube api key

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,9 +1,11 @@
-const YOUTUBE_API_KEY = "AIzaSyBB6FfrKm28DNebmCGhv0hO6iiG5vv4n-U";
+import { YOUTUBE_API_KEY } from "./youtubeApiKey";
 
+// VIDEO API
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&part=contentDetails&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   YOUTUBE_API_KEY;
 
+// YOUTUBE CHANNEL API
 export const YOUTUBE_CHANNEL_ID_API =
-  "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=" +
+  "https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=channelId&maxResults=50&key=" +
   YOUTUBE_API_KEY;
