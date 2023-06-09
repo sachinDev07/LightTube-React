a48e1ac17cf65399165ada1bd2a13ac1a5a8681e
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-09 20:33:01 +0500

    fixed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 import { YOUTUBE_API_KEY } from "./youtubeApiKey";
 
-// VIDEO API
+// VIDEOS API
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&part=contentDetails&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   YOUTUBE_API_KEY;
