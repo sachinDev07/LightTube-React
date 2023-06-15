a250f54c8cccd2800513a5737732754ef20fdadf
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-15 21:51:11 +0500

    some changes in the Shimmer Component

--- src/components/Shimmer/Shimmer.jsx
+++ src/components/Shimmer/Shimmer.jsx
@@ -1,21 +1,20 @@
-
 const Shimmer = () => {
   return (
-    <div className="mt-8 flex w-full flex-wrap ">
-        {Array(18)
-          .fill(" ")
-          .map((item, index) => (
-            <div key={index} className=" mr-6 h-[342px] w-[360px]">
-              <div className="mb-2 h-[202px] w-[360px] rounded-md bg-[#686868]"></div>
-              <div className="mt-4 flex h-[137px] justify-center gap-4">
-                <div className="h-[40px] w-[40px] rounded-3xl bg-[#686868]"></div>
-                <div className="w-[290px]">
-                  <div className="mb-1 h-5 w-full rounded-sm bg-[#686868]"></div>
-                  <div className=" mt-4 h-5 w-[180px] rounded-sm bg-[#686868]"></div>
-                </div>
+    <div className="mt-8 flex w-full flex-wrap md:justify-between  lg:ml-[0px] lg:gap-2 ">
+      {Array(18)
+        .fill(" ")
+        .map((item, index) => (
+          <div key={index} className="w-full md:w-[300px] lg:w-[340px]">
+            <div className="mb-2 h-[280px] bg-[#686868] md:h-[200px] md:rounded-md"></div>
+            <div className="mt-4 flex h-[180px] justify-between gap-4 px-4 md:h-[137px]">
+              <div className="h-[50px] w-[50px] rounded-3xl bg-[#686868] md:h-[40px] md:w-[40px]"></div>
+              <div className="w-[350px] md:w-[290px]">
+                <div className="mb-1 h-5 w-full rounded-sm bg-[#686868]"></div>
+                <div className=" mt-4 h-5 w-[220px] rounded-sm bg-[#686868] md:w-[180px]"></div>
               </div>
             </div>
-          ))}
+          </div>
+        ))}
     </div>
   );
 };
