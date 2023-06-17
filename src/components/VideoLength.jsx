c5897b82061a11f9c6ad6ab0987fca06ca6ba8f8
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-17 06:50:29 +0500

    Video length component is done

---
+++ src/components/VideoLength.jsx
@@ -0,0 +1,26 @@
+const VideoLength = ({ videoLength }) => {
+  return (
+    <span className="absolute bottom-3 right-2 rounded-md bg-black px-1 text-center text-sm font-semibold text-white">
+      {videoLength}
+    </span>
+  );
+};
+
+export default VideoLength;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
