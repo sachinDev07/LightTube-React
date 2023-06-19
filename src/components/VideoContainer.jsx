06546f9a79a566d4b0fdc9e1db53dc9dde32de22
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-19 22:23:05 +0500

    infinite scrolling is done

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import { YOUTUBE_VIDEOS_API } from "../utils/constant";
 import Video from "./Videos";
 import Shimmer from "./Shimmer/Shimmer";
+import { YOUTUBE_API_KEY } from "../utils/youtubeApiKey";
+
+const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&part=contentDetails&chart=mostPopular&maxResults=50&regionCode=IN`;
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState('');
+  const [prevPageToken, setPrevPageToken] = useState('');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -16,15 +20,35 @@ const VideoContainer = () => {
   const getVideos = async () => {
     try {
       setLoading(true);
-      const response = await fetch(YOUTUBE_VIDEOS_API);
+      const response = await fetch(
+        `${YOUTUBE_VIDEOS_API}&pageToken=${nextPageToken}&prevPageToken=${prevPageToken}&key=${YOUTUBE_API_KEY}`
+      );
       const data = await response.json();
-      setVideos(data?.items);
+      setVideos(prevVideos => [...prevVideos, ...data.items]);
+      setNextPageToken(data.nextPageToken);
+      setPrevPageToken(data.prevPageToken);
+      console.log(data);
     } catch (error) {
-      console.error(error);
+      console.error('Error fetching videos:', error);
     } finally {
       setLoading(false);
     }
   };
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    const visibleHeight = document.documentElement.clientHeight;
+    const totalHeight = document.documentElement.scrollHeight;
+
+    if (scrollY + visibleHeight >= totalHeight * 0.8) {
+      getVideos();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   if (loading) {
     return <Shimmer />;
@@ -32,9 +56,10 @@ const VideoContainer = () => {
 
   return (
     <div className="mt-6 flex w-full flex-wrap md:ml-24  md:gap-3 lg:ml-[0px] lg:justify-between lg:gap-0">
-      {videos?.map((video) => {
+      {videos?.map((video, index) => {
+        const uniqueKey = `${video.id}-${index}`;
         return (
-          <Link to={`watch?v=`+ video.id} key={video.id}>
+          <Link to={`watch?v=`+ video.id} key={uniqueKey}>
             <Video  video={video} />
           </Link>
         );
