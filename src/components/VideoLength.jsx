ac5e59984ab510493055363e448c63f47a586b3d
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-24 22:36:28 +0500

    fixes the css

--- src/components/VideoLength.jsx
+++ src/components/VideoLength.jsx
@@ -1,26 +1,9 @@
 const VideoLength = ({ videoLength }) => {
   return (
-    <span className="absolute bottom-3 right-2 rounded-md bg-black px-1 text-center text-sm font-semibold text-white">
+    <span className="absolute bottom-3 right-0 mr-2 rounded-md bg-black p-1 text-center text-sm font-semibold text-white">
       {videoLength}
     </span>
   );
 };
 
 export default VideoLength;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
