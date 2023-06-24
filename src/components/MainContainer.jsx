b2d94d6ec88f2e8257da945925112055e8aa46e4
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-24 22:34:08 +0500

    some changes in the css

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -8,7 +8,7 @@ const MainContainer = () => {
 
   return (
     <div
-      className={`w-full md:w-[85%] lg:w-[90%]    ${
+      className={`w-full md:w-[85%] lg:w-[90%]  ${
         isMenuOpen === true ? "md:opacity-50 lg:ml-72 lg:pr-28" : "lg:ml-24"
       } lg:opacity-100 `}
     >
