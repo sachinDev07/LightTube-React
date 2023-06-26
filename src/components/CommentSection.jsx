575961b582b1fd04f8b372b9257e285832776664
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-26 14:51:03 +0500

    comment section

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { commentsData } from "../utils/constant";
+import CommentsList from "./CommentsList";
+
 const CommentSection = () => {
-  return <div className="col-span-8">Comment</div>;
+  return (
+    <div className="col-span-8 ml-14 mt-1 pr-4 py-2">
+      <div className="rounded-lg bg-[#363535] py-2">
+        <h1 className="ml-2 text-2xl font-bold">Comment :</h1>
+        <div className="mt-1 p-2 ">
+          <CommentsList comments={commentsData} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentSection;
