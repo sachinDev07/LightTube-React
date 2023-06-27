9bbe79e22e064ea5aab9164b18d9f061f3673292
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-27 13:33:42 +0500

    added LiveChat Component

--- src/components/RecommendedVideo.jsx
+++ src/components/RecommendedVideo.jsx
@@ -1,7 +1,11 @@
-
+import LiveChat from "./LiveChat";
 
 const RecommendedVideo = () => {
-  return <div className=" col-span-4 row-span-6">Recommended Videos</div>;
+  return (
+    <div className=" col-span-4 row-span-6">
+      <LiveChat />
+    </div>
+  );
 };
 
 export default RecommendedVideo;
