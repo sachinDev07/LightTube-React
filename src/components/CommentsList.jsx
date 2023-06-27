d887a3a1df41b767e205a6b5f02d4aa577fa619e
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-27 13:32:19 +0500

    just commit

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,14 +1,14 @@
 import Comments from "./Comments";
 
 const CommentsList = ({ comments }) => {
-    return comments.map((comment, index) => (
-      <div className="">
-        <Comments key={index} data={comment} />
-        <div className="ml-12 border-l-2 border-yellow-500 ">
-          <CommentsList comments={comment.replies} />
-        </div>
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comments data={comment} />
+      <div className="ml-12 border-l-2 border-yellow-500 ">
+        <CommentsList comments={comment.replies} />
       </div>
-    ));
-  };
+    </div>
+  ));
+};
 
-  export default CommentsList;
+export default CommentsList;
