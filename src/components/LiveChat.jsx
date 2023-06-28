9a9c2c6162e904f190e4d66e41cb4ff469aad576
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-28 13:27:19 +0500

    live typing message is complete in Live chat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import ChatMessages from "./ChatMessages";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,6 +9,8 @@ const LiveChat = () => {
   const dispatch = useDispatch();
   const chatMessage = useSelector((store) => store.chat.messages);
 
+  const [liveMessage, setLiveMessage] = useState("");
+
   useEffect(() => {
     const interval = setInterval(() => {
       // API polling
@@ -18,16 +20,56 @@ const LiveChat = () => {
           chatMessage: generateRandomMessage() + " 😲",
         })
       );
-    }, 400);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="mr-4 mt-6 h-[480px] overflow-y-scroll rounded-md border-2 border-white p-2 flex flex-col-reverse">
-      {chatMessage.map((c, index) => (
-        <ChatMessages key={index} name={c.name} chatMessage={c.chatMessage} />
-      ))}
+    <div className="fixed mr-6 rounded-md border-2 border-white">
+      <div className="mr-1 mt-6 flex h-[480px] flex-col-reverse overflow-y-scroll p-2">
+        {chatMessage.map((c, index) => (
+          <ChatMessages key={index} name={c.name} chatMessage={c.chatMessage} />
+        ))}
+      </div>
+      <form
+        className="flex w-[450px] items-center justify-between p-2 px-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Sachin kumar",
+              chatMessage: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <div className="w-[340px] rounded-lg border-2 border-green-600 px-2 py-1">
+          <input
+            className="w-full border-none bg-transparent outline-none"
+            type="text"
+            placeholder="let's chat"
+            value={liveMessage}
+            onChange={(e) => setLiveMessage(e.target.value)}
+          />
+        </div>
+        <button
+          className="rounded-md bg-green-600 px-3 py-1 hover:bg-green-800"
+          type="submit"
+          onSubmit={() => {
+            dispatch(
+              addMessage({
+                name: "Sachin kumar",
+                chatMessage: liveMessage,
+              })
+            );
+            setLiveMessage("");
+          }}
+        >
+          Send
+        </button>
+      </form>
     </div>
   );
 };
